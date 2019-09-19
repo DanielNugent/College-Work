00a10d3a0919998cf8f2482f81f64fa6dc3849fd
rvailnaveed <37183938+rvailnaveed@users.noreply.github.com>
2019-09-19 13:09:27 +0100

    Start Comp Arch T1

---
+++ 3rd Year/Computer Architecture/t1Test/t1Test/t1.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+extern "C" int _cdecl min(int, int, int);		// _cdecl calling convention
+extern "C" int _cdecl p(int, int, int, int);   // _cdecl calling convention
+extern "C" int _cdecl gcd(int, int);
+extern "C" int g;
