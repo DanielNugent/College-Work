3bc2152bb00899152da508986199ad2dd27d86bf
Rvail <naveedm@tcd.ie>
2019-10-23 18:52:41 +0100

    Finished haskell assignment

--- 3rd Year/Functional Programming/Exercise02/src/Ex02.hs
+++ 3rd Year/Functional Programming/Exercise02/src/Ex02.hs
@@ -1,4 +1,4 @@
-{- butrfeld Andrew Butterfield -}
+{- naveedm Muhammad Rvail Naveed -}
 module Ex02 where
 import Data.List ((\\))
 
@@ -26,7 +26,7 @@ define d s v = (s,v):d
 find :: Eq k => Dict k d -> k -> Maybe d
 find []             _                 =  Nothing
 find ( (s,v) : ds ) name | name == s  =  Just v
-                         | otherwise  =  find ds name
+                          | otherwise  =  find ds name
 
 type EDict = Dict String Double
 
@@ -41,39 +41,63 @@ v42 = Val 42 ; j42 = Just v42
   -- (2) the expression contains a variable not in the dictionary.
 
 eval :: EDict -> Expr -> Maybe Double
-eval d e = Just 1e-99
+eval _ (Val x) = Just x
+eval d (Var x) = find d x
+
+eval d (Add x y) = case	(eval d x, eval d y) of
+                   (Just a, Just b) -> Just (a + b)
+                   _		 -> Nothing
+
+eval d (Sub x y) = case	(eval d x, eval d y) of
+                   (Just a, Just b) -> Just (a - b)
+                   _		 -> Nothing
+ 
+eval d (Mul x y) = case (eval d x,eval d y) of
+                   (Just a, Just b)  ->  Just (a * b)
+                   _    ->  Nothing
+
+eval d (Dvd x y) = case (eval d x, eval d y) of
+                   (Just a, Just b) ->  if b == 0.0 then Nothing else Just (a / b) 
+                   _   ->  Nothing
+
+eval d (Def x y z) = case eval d y of
+                     Nothing  ->  Nothing
+                     Just a  ->  eval (define d x a) z
 
 -- Part 1 : Expression Laws -- (15 test marks, worth 15 Exercise Marks) --------
 
 {-
-
 There are many, many laws of algebra that apply to our expressions, e.g.,
-
   x + y            =  y + z         Law 1
   x + (y + z)      =  (x + y) + z   Law 2
   x - (y + z)      =  (x - y) - z   Law 3
   (x + y)*(x - y)  =  x*x - y*y     Law 4
   ...
-
   We can implement these directly in Haskell using Expr
-
   Function LawN takes an expression:
     If it matches the "shape" of the law lefthand-side,
     it replaces it with the corresponding righthand "shape".
     If it does not match, it returns Nothing
-
     Implement Laws 1 through 4 above
 -}
 
-
 law1 :: Expr -> Maybe Expr
-law1 e = j42
+law1 e = case(e) of
+         (Add x y) -> Just(Add y x)
+         _	  -> Nothing
 
 law2 :: Expr -> Maybe Expr
-law2 e = j42
+law2 e = case(e) of
+         (Add x (Add y z)) -> Just(Add (Add x y) z)
+         _		  -> Nothing
 
 law3 :: Expr -> Maybe Expr
-law3 e = j42
+law3 e = case(e) of
+         (Sub x (Add y z)) -> Just(Sub (Sub x y) z)
+         _		  -> Nothing
 
 law4 :: Expr -> Maybe Expr
-law4 e = j42
+law4 e = case(e) of
+         (Mul (Add x y) (Sub p q)) -> if x==p && y==q then Just(Sub (Mul x x) (Mul y y))
+                                         else Nothing
+         _			  -> Nothing  
