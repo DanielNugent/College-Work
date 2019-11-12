5fd14ebafec640db54d307d6e74ca59908fda8dc
Rvail <naveedm@tcd.ie>
2019-11-12 18:43:42 +0000

    (writers, cast, episodes) tables and fixed circular relation between characters and cast

--- 3rd Year/Information Management II/bb.sql
+++ 3rd Year/Information Management II/bb.sql
@@ -9,11 +9,10 @@ CREATE TABLE `Characters`(
     `character_id` int(4) NOT NULL PRIMARY KEY, -- Make this auto_increment instead
     `name` varchar(50) NOT NULL,
     `cast_ssn` int(4) NOT NULL,
-    `status` varchar(7) NOT NULL, -- Need to make trigger before insert to check if value in (dead, alive, unknown) 
-    `role` varchar(20) NOT NULL,
     `alias` varchar(20),
+    `status` varchar(7) NOT NULL, -- Need to make trigger before insert to check if value in (dead, alive, unknown) 
 
-    CONSTRAINT FK_cast_ssn FOREIGN KEY (`cast_ssn`) REFERENCES `Cast` (`ssn`)
+    CONSTRAINT FK_cast_ssn FOREIGN KEY (`cast_ssn`) REFERENCES `Cast` (`cast_ssn`)
 );
 
 -- ---------------------
@@ -22,7 +21,7 @@ CREATE TABLE `Characters`(
 
 BEGIN;
 
-INSERT INTO `Characters` VALUES (1111, 'Walter White', 9999, 'DEAD', 'Meth Kingpin', 'Heisenberg');
+INSERT INTO `Characters` VALUES (1111, 'Walter White', 9999, 'Heisenberg', 'DEAD');
 
 COMMIT;
 
@@ -31,16 +30,43 @@ COMMIT;
 -- ---------------------
 DROP TABLE IF EXISTS `Cast`;
 CREATE TABLE `Cast`(
-    `ssn` int(4) NOT NULL PRIMARY KEY, -- Make this auto_increment instead
+    `cast_ssn` int(4) NOT NULL PRIMARY KEY, -- Make this auto_increment instead
     `name` varchar(50) NOT NULL,
-    `character_id` int(4) NOT NULL,
+    `first_appearance` int NOT NULL,
 
-    CONSTRAINT FK_character_id FOREIGN KEY (`character_id`) REFERENCES `Characters` (`character_id`)
+    CONSTRAINT FK_appearance_id FOREIGN KEY (`first_appearance`) REFERENCES `Episodes` (`episode_id`)
 );
 
 BEGIN;
 
-INSERT INTO `Cast` VALUES (9999, 'Bryan Cranston', 1111);
+INSERT INTO `Cast` VALUES (9999, 'Bryan Cranston', 11);
+
+COMMIT;
+
+
+-- ---------------------
+-- Episodes Table --
+-- ---------------------
+DROP TABLE IF EXISTS `Episodes`;
+CREATE TABLE `Episodes`(
+    `episode_id` int(2) NOT NULL PRIMARY KEY,
+    `season` int(1) NOT NULL,
+    `mins_long` int(2) NOT NULL,
+    `air_date` DATE NOT NULL,
+    `written_by` int(4) NOT NULL,
+
+    CONSTRAINT FK_season_id FOREIGN KEY (`season`) REFERENCES `Seasons` (`season_no`),
+    CONSTRAINT FK_writer_id FOREIGN KEY (`written_by`) REFERENCES `Writers` (`writer_ssn`)
+
+);
+
+-- ---------------------
+-- Init 'Episodes' --
+-- ---------------------
+
+BEGIN;
+
+INSERT INTO `Episodes` VALUES (11, 1, 54, '2008-01-20', 3456);
 
 COMMIT;
 
@@ -65,5 +91,23 @@ INSERT INTO `Seasons` VALUES (2, '2009-03-08', '2009-05-31', 13);
 INSERT INTO `Seasons` VALUES (3, '2010-03-21', '2010-06-13', 13);
 INSERT INTO `Seasons` VALUES (4, '2011-07-17', '2011-10-09', 13);
 INSERT INTO `Seasons` VALUES (5, '2012-07-15', '2013-09-29', 16);
+
+COMMIT;
+
+-- ---------------------
+-- Writers Table --
+-- ---------------------
+DROP TABLE IF EXISTS `Writers`;
+CREATE TABLE `Writers`(
+    `writer_ssn` int(4) NOT NULL PRIMARY KEY,
+    `name` varchar(20) NOT NULL -- trigger to check if writer is part of writers list
+);
+
+-- ---------------------
+-- Init 'Writers' --
+-- ---------------------
+BEGIN;
+
+INSERT INTO `Writers` VALUES (3456, 'Vince Gilligan');
 
 COMMIT;
